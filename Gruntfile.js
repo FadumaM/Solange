94cd11c8ba33c4836c9dcefd553fbd727507c296
FadumaM <faduma.mohammed@gmail.com>
2016-12-09 11:22:42 +0000

    created files structure

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: [
+        'public/src/js/**/*.js',
+        '!public/src/js/_bower.js',
+      ],
+    },
+    bower_concat: {
+      all: {
+        dest: {
+          'js': 'public/src/js/_bower.js',
+          'css': 'public/src/scss/_bower.scss'
+        },
+        mainFiles: {
+          bootstrap: [
+            'dist/js/bootstrap.js',
+            'dist/css/bootstrap.css'
+          ]
+        }
+      },
+    },
+    sass: {
+      expanded: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'public/css/app.css': 'public/src/scss/app.scss'
+        }
+      },
+      compressed: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'public/css/app.min.css': '/public/src/scss/app.scss'
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          'public/src/js/_bower.js',
+          'public/src/js/app.js',
+          'public/src/js/**/*.js'
+        ],
+        dest: 'public/js/app.js'
+      }
+    },
+    uglify: {
+      'public/js/app.min.js': 'public/js/app.js'
+    },
+    watch: {
+      configFiles: {
+        files: ['Gruntfile.js', 'package.json'],
+        options: {
+          reload: true
+        }
+      },
+      scss: {
+        files: ['public/src/scss/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      },
+      js: {
+        files: ['public/src/js/**/*.js'],
+        tasks: ['jshint', 'concat', 'uglify'],
+        options: {
+          livereload: true
+        }
+      },
+      index: {
+        files: ['index.html'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    replace: {
+      production: {
+        options: {
+          patterns: [{
+            match: /app\.js/,
+            replacement: 'app.min.js'
+          }, {
+            match: /app\.css/,
+            replacement: 'app.min.css'
+          }]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['index.html']
+        }]
+      },
+      development: {
+        options: {
+          patterns: [{
+            match: /app\.min\.js/,
+            replacement: 'app.js'
+          }, {
+            match: /app\.min\.css/,
+            replacement: 'app.css'
+          }]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['index.html']
+        }]
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['sass:expanded', 'concat', 'watch']);
+  grunt.registerTask('deploy', ['sass:compressed', 'concat', 'uglify']);
+};
---
+++ public/src/js/app.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  var token;
+  getToken();
+
+  function getToken() {
+   $.ajax({
+    method: "GET",
+     url: "http://localhost:3000/getToken"
+   }).done(function(data) {
+     token = data;
+     getTrack(token);
+   });
+  }
+
+  function getTrack(token) {
+   $.ajax({
+     url: "http://localhost:3000/music/"+token+"/tracks",
+     method: "GET",
+   }).done(function(data) {
+     console.log(data);
+   });
+  }
+
+
+};
